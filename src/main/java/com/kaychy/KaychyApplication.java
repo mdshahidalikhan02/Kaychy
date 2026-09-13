0c4c32f6c615e1976742e773ac9112b2ff7803f7
ShahidKhan <btech10263.19@bitmesra.ac.in>
2026-09-13 19:16:09 +0530

    kaychy- setup

--- src/main/java/com/kaychy/KaychyApplication.java
+++ src/main/java/com/kaychy/KaychyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KaychyApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(KaychyApplication.class, args);
     }
